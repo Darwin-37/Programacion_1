1a2f8753a7469065a0d4ce1b82e004e2b482d33a
Darwin-37 <djchara@estudiante.uniajc.edu.co>
2024-09-30 20:36:31 +0000

    finalizacion ejercicio 5.6

--- Clase30sep2024/ejercicio56.java
+++ Clase30sep2024/ejercicio56.java
@@ -1,7 +1,24 @@
-public class ejercicio56 {
-    public static void main (String []args){
+import java.util.Scanner;
 
-    System.out.println ("Mi primer programa de JAVA en Github");
-    
+public class ejercicio56 {
+    public static void main(String[] args) {
+        double nota = 0.0;
+        int aprobados = 0, noAprobados = 0;
+        // Notas de los estudiantes
+        Scanner leer = new Scanner(System.in);
+        System.out.print("Digite la cantidad de estudiantes:");
+        int estudiantes = leer.nextInt();
+
+        for (int i = 1; i <= estudiantes; i++) {
+            System.out.println("Nota del estudiante " + i + ":");
+            nota = leer.nextDouble();
+
+            if (nota >= 5.0) {
+                aprobados++;
+            } else {
+                noAprobados++;
+            }
+        }
+        System.out.println(" Aprobados:" + "aprobados" + noAprobados);
     }
 }
