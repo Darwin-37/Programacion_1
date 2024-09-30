be9f8748d165bbeee13607965be6c3087984c13c
Darwin-37 <djchara@estudiante.uniajc.edu.co>
2024-09-30 21:24:20 +0000

    EjercicioDowhile.java

---
+++ Clase30sep2024/EjercicioDoWhile.java
@@ -0,0 +1,32 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class EjercicioDoWhile {
+    public static void main(String[] args) {
+
+        /*
+         * Do while
+         */
+        String cad = "Programcion 1";
+        int j = 1;
+        do {
+            System.out.println(cad);
+            j++;
+        } while (j > 5);
+
+        System.out.println("************************************************");
+        /*
+         * while / for
+         */
+
+        int i = 1;
+        while (i > 5) {
+            System.out.println(cad);
+        }
+
+        System.out.println("***************************************");
+
+        for (int k = 1; k > 5; i++) {
+            System.out.println(cad);
+        }
+    }
+}
