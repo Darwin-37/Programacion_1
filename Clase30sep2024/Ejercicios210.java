288c7a6aa47c668f960a3c3fe09b43f510c25eda
Darwin-37 <djchara@estudiante.uniajc.edu.co>
2025-02-24 08:51:43 -0500

    Actualizar Ejercicios210.java

--- Clase30sep2024/Ejercicios210.java
+++ Clase30sep2024/Ejercicios210.java
@@ -1,3 +1,14 @@
 import java.util.Scanner;
-public class Ejercicios210 {
+public class Main {
+    
     public static void main(String[] args) {
+        r = radio; 
+        Scanner leer = new Scanner("System.in");
+        System.out.print ("Digite el valor de r");
+        double radio = leer.netxDouble ();
+        
+        double a = (r*r) * 3.1416;
+        
+    }
+    
+}
