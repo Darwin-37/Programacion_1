f83017cb7f14e88a03195919c530f31f2cb91aa8
Darwin-37 <djchara@estudiante.uniajc.edu.co>
2025-02-24 17:14:38 +0000

    Ejercicio 2 11

---
+++ Clase30sep2024/main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class main {
+    
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        System.out.print ("Digite el valor de r");
+        double radio = leer.nextDouble ();
+        
+        double l = 3.14 * (radio * radio);
+        double a = (radio * radio) * 3.1416;
+        
+        System.out.print ("La longitud de la circunferencia es : " +l+ " y el area de la misma circunfere
+        
+        scanner.close();
+        
+    }
+    
+}
