1c7d72595b43609f6746e072186ee9db10af340f
Darwin-37 <djchara@estudiante.uniajc.edu.co>
2025-04-21 20:11:00 +0000

    Creacion de la clase Circulo

---
+++ Clase30sep2024/Operaciones.java/Clase11POO/Circulo.java
@@ -0,0 +1,21 @@
+public class Circulo {
+
+    // Atributos
+    private float radio;
+
+    // Constructor de la clase : nos permite inicializar la clase
+    // El metodo del constructor se llama igual que la clase
+    public Circulo(float radio) {
+        this.radio = radio; // this : es una autoreferencia al atributo de la clase 
+    }
+
+    // Metodos
+    public double calcularPerimetro() {
+        return 2 * Math.PI * radio;
+    }
+
+    public double calcularArea() {
+        return Math.PI * (radio * radio); // Math.PI * Math.pow(radio,2)
+    }
+
+}
