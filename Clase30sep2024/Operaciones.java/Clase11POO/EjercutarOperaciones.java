d4300129d5199140e12d48963f04d684d908a508
Darwin-37 <djchara@estudiante.uniajc.edu.co>
2025-04-27 23:23:47 +0000

    Actualizacion del ejecutar operaciones

---
+++ Clase30sep2024/Operaciones.java/Clase11POO/EjercutarOperaciones.java
@@ -0,0 +1,14 @@
+public class EjercutarOperaciones {
+    public static void main(String[] args) {
+        OperacionesBasicas operaciones = new OperacionesBasicas();
+
+        double num1 = 10;
+        double num2 = 5;
+
+        System.out.println("Suma: " + operaciones.sumar(num1, num2));
+        System.out.println("Resta: " + operaciones.restar(num1, num2));
+        System.out.println("Multiplicación: " + operaciones.multiplicar(num1, num2));
+        System.out.println("División: " + operaciones.dividir(num1, num2));
+    }
+}
+    
