a79b9ab1595234fa9b27885b41c5804819a5f0eb
Darwin-37 <djchara@estudiante.uniajc.edu.co>
2025-05-05 19:20:36 +0000

    Creacion del escenario 2

--- Clase30sep2024/Operaciones.java/Clase11POO/CajeroElectronico.java
+++ Clase30sep2024/Operaciones.java/Clase11POO/CajeroElectronico.java
@@ -1,47 +1,60 @@
 public class CajeroElectronico {
-    // Atributos
+    //Atributos
     private double saldo;
-
-    // Constructor
-    public CajeroElectronico(double saldo) {
+   
+    //Constructor: Permite inicializar la clase
+    public CajeroElectronico(double saldo){
         this.saldo = saldo;
     }
-
-    // Metodos
-    // Obtiene lo que tenga asignado el atributo saldo
-    // return saldo
-    // param cantidad
-    public double getSaldo() {
+ 
+    //-----------------Métodos---------------------
+ 
+    /**
+     * Obtiene lo que tenga asignado el atributo saldo
+     * @return saldo
+     */
+    public double getSaldo(){
         return saldo;
     }
-
-    public double depositar(double cantidad) {
+ 
+    /**
+     * Método para depositar dinero y asignarselo al saldo
+     * @param cantidad
+     * @return saldo
+     */
+    public double depositar(double cantidad){
         return saldo += cantidad;
     }
-
-    public void retirar(double cantidad) {
-        saldo -= cantidad;
-        if (cantidad <= saldo) {
-            System.out.println("El nuevo saldo es : " + saldo);
-        } else {
-            System.out.println(" La cantidad a retirar es mayor que el saldo ");
+ 
+    /**
+     * Método retirar permite restarle al saldo el monto retirado
+     * @param cantidad
+     */
+    public void retirar(double cantidad){
+        if(cantidad <= saldo){
+            saldo -= cantidad;
+            System.out.println("El nuevo saldo es: " + saldo);
+        }else{
+            System.out.println("La cantidad a retirar es mayor que el saldo");    
         }
     }
-    // Obtener el saldo de la cuenta
-    // return saldo
-
-    public double consultarSaldo(double cantidad) {
+ 
+    /**
+     * Obtener el saldo actual de la cuenta
+     * @return saldo
+     */
+    public double consultarSaldo(){
         return saldo;
-
     }
-
-    public void transferir(double cantidad, String destino) {
-        saldo -= cantidad;
-        if (cantidad <= saldo) {
-            System.out.println("Se realizo una transferencia por: " + cantidad +
-                    "a:" + destino + " su saldo es de : " + saldo);
-        } else {
-            System.out.println("La transferencia nose pudo realizar");
+ 
+    public void transferir(double cantidad, String destino){
+        if(cantidad <= saldo){
+            saldo -= cantidad;
+            System.out.println("Se realizó una transferencia por: " + cantidad +
+            " a: " + destino + " y su saldo es: " + saldo);
+        }else{
+            System.out.println("La transferencia no se pudo hacer");
         }
     }
+ 
 }
